c9ee1c7a0bbe0dee33219b6a703525d986ceb464
Kris Jones <kristop.jones@gmail.com>
2021-08-23 11:33:22 -0600

    workspace

---
+++ sqwuakClient/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-1s3xbyye.us.auth0.com'
+export const audience = 'https://sqwuakapp'
+export const clientId = 'hSxbMAEyIXY0Ayzs4yAkJzVRMSfhIt4c'
