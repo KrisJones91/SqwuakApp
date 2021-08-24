1904d458663d02ad8b269837c173a76ab4bd5485
Kris Jones <kristop.jones@gmail.com>
2021-08-24 12:00:31 -0600

    archives service put request

--- sqwuakClient/src/services/ArchivesService.js
+++ sqwuakClient/src/services/ArchivesService.js
@@ -11,5 +11,24 @@ class ArchivesService {
     const res = await api.get('api/archives/' + id)
     AppState.activeArchive = res.data
   }
+
+  async getPostsByArchiveId(id) {
+    const res = await api.get('api/archives/' + id + '/posts')
+    AppState.activeArchivePosts = res.data
+  }
+
+  async createArchive(archive) {
+    const res = await api.post('api/archives', archive)
+    AppState.myArchives.push(res.data)
+    AppState.profileArchives.push(res.data)
+  }
+
+  async editArchive(id, editedArch) {
+    const res = await api.put('api/archives/' + id, editedArch)
+    const myIndex = AppState.myArchives.indexOf(archive => archive.id === id)
+    AppState.myArchives = AppState.myArchives.splice(myIndex, 1, res.data)
+    const profIndex = AppState.profileArchives.indexOf(archive => archive.id === id)
+    AppState.profileArchives = AppState.profileArchives.splice(profIndex, 1, res.data)
+  }
 }
 export const archivesService = new ArchivesService()
